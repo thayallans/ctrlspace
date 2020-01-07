99a1350393bbfc4afe0b3c25fc5422a879181d84
Thayallan Srinathan <thayallan.srinathan@gmail.com>
2020-01-07 18:23:25 -0500

    new stuff

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -11,6 +11,7 @@ const config = merge(common, {
     popup: PATHS.src + '/popup.js',
     contentScript: PATHS.src + '/contentScript.js',
     background: PATHS.src + '/background.js',
+    shortcut_map: PATHS.src + '/shortcut_map.js'
   },
 });
 
