18b4e90042e1235a6f8036ce5dd2d9c1cfce0394
Thayallan Srinathan <thayallan.srinathan@gmail.com>
2020-01-11 12:48:22 -0500

    minor bug fixes

--- src/popup.js
+++ src/popup.js
@@ -10,103 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
     $('head').append(link);
   }
 });
-//  let file = '';
-//  if(result.site.includes('airtable.com')) {
-//    file = 'mac_content/mac_airtable.json'
-//  } else if(result.site.includes('asana.com')) {
-//    file = 'mac_content/mac_asana.json'
-//  } else if(result.site.includes('discordapp.com')) {
-//    file = 'mac_content/mac_discord.json'
-//  } else if(result.site.includes('evernote.com')) {
-//    file = 'mac_content/mac_evernote.json'
-//  } else if(result.site.includes('figma.com')) {
-//    file = 'mac_content/mac_figma.json'
-//  } else if(result.site.includes('framer.com')) {
-//    file = 'mac_content/mac_framer-x.json'
-//  } else if(result.site.includes('github.com')) {
-//    file = 'mac_content/mac_github.json'
-//  } else if(result.site.includes('drive.google.com')) {
-//    file = 'mac_content/mac_google-drive.json'
-//  } else if(result.site.includes('jira.com')) {
-//    file = 'mac_content/mac_jira.json'
-//  } else if(result.site.includes('monday.com')) {
-//    file = 'mac_content/mac_monday.json'
-//  } else if(result.site.includes('notion.so')) {
-//    file = 'mac_content/mac_notion.json'
-//  } else if(result.site.includes('proto.io')) {
-//    file = 'mac_content/mac_proto-io.json'
-//  } else if(result.site.includes('quip.com')) {
-//    file = 'mac_content/mac_quip.json'
-//  } else if(result.site.includes('slack.com')) {
-//    file = 'mac_content/mac_slack.json'
-//  } else if(result.site.includes('trello.com')) {
-//    file = 'mac_content/mac_trello.json'
-//  } else if(result.site.includes('youtube.com')) {
-//    file = 'mac_content/mac_youtube.json'
-//  }
-//  $.getJSON(file, function(json) {
-//    const all_sections = json.sections;
-//    let all_shortcuts = [];
-//    const main_div = document.getElementById('mainDiv');
-//    all_sections.forEach((section) => {
-//      section.shortcuts.forEach((shortcut) => {
-//        var outer_div = document.createElement('div');
-//        outer_div.classList.add('flex');
-//        var first_outer_div = document.createElement('div');
-//        first_outer_div.classList.add('w-1/2');
-//        first_outer_div.classList.add('bg-gray-900');
-//        first_outer_div.classList.add('h-12');
-//        var first_inner_div = document.createElement('div');
-//        first_inner_div.classList.add('px-6');
-//        first_inner_div.classList.add('py-4');
-//        var inner_p = document.createElement('p');
-//        inner_p.classList.add('text-lg');
-//        inner_p.classList.add('font-semibold');
-//        inner_p.classList.add('text-gray-100');
-//        inner_p.style.textAlign = 'center';
-//        inner_p.innerText = shortcut.description;
-//        var second_outer_div = document.createElement('div');
-//        second_outer_div.classList.add('w-1/2');
-//        second_outer_div.classList.add('bg-gray-900');
-//        second_outer_div.classList.add('h-12');
-//        var second_inner_div = document.createElement('div');
-//        second_inner_div.classList.add('px-6');
-//        second_inner_div.classList.add('py-4');
-//        var second_even_inner_div = document.createElement('div');
-//        second_even_inner_div.style.textAlign = 'left'
-//        shortcut.keys.forEach((key) => {
-//          var key_span = document.createElement('span');
-//          key_span.classList.add('inline-block');
-//          key_span.classList.add('bg-gray-200');
-//          key_span.classList.add('rounded-full');
-//          key_span.classList.add('px-4');
-//          key_span.classList.add('mx-1');
-//          key_span.classList.add('py-1');
-//          key_span.classList.add('text-sm');
-//          key_span.classList.add('font-semibold');
-//          key_span.classList.add('text-gray-700');
-//          key_span.innerText = key;
-//          second_even_inner_div.appendChild(key_span);
-//        });
-//    
-//        first_inner_div.appendChild(inner_p);
-//        first_outer_div.appendChild(first_inner_div);
-//        second_inner_div.appendChild(second_even_inner_div);
-//        second_outer_div.appendChild(second_inner_div);
-//        outer_div.appendChild(first_outer_div);
-//        outer_div.appendChild(second_outer_div)
-//        main_div.appendChild(outer_div);
-//        all_shortcuts.push(shortcut);
-//      });
-//    });
-//  });
-//  //chrome.storage.sync.get(['logged_in'], (result) => {
-//  //  if(result.logged_in == 'false') {      
-//  //    window.location.href = "login.html";
-//  //  }
-//  //  return true;
-//  //});
-//});
 
 document.addEventListener("keydown", function (event) {
   let input, filter, ul, li, a, i, txtValue;
@@ -318,36 +221,38 @@ document.addEventListener("keydown", function (event) {
 function filter_words() {
   var input, filter, a, i, txtValue;
   input = document.getElementById("search");
-  filter = input.value.toUpperCase();
-  var elements = document.getElementsByClassName('w-1/2 bg-gray-900 h-12');
-  var current_elements = [];
-  for (i = 0; i < elements.length; i++) {
-    if(elements[i].getElementsByTagName('p')[0]) {
-      elements[i].parentElement.tabIndex = i/2;
-      a = elements[i].getElementsByTagName('p')[0];
-      txtValue = a.innerText;
-      const words = txtValue.split(" ");
-      if(txtValue.toUpperCase().indexOf(filter) > -1) {
-        elements[i].parentElement.style.display = "";
-        if(!current_elements.includes(txtValue)){
-          current_elements.push(txtValue);
-        }
-      } else {
-        let total = 0;
-        words.forEach((word) => {
-          if (smart_search(filter.toLowerCase(), word.toLowerCase())) {
-            elements[i].parentElement.style.display = "";
-            if(!current_elements.includes(txtValue)) {
-              current_elements.push(txtValue);
-              total++;
-            }
+  if(input != undefined) {
+    filter = input.value ? input.value.toUpperCase() : null;
+    var elements = document.getElementsByClassName('w-1/2 bg-gray-900 h-12');
+    var current_elements = [];
+    for (i = 0; i < elements.length; i++) {
+      if(elements[i].getElementsByTagName('p')[0]) {
+        elements[i].parentElement.tabIndex = i/2;
+        a = elements[i].getElementsByTagName('p')[0];
+        txtValue = a.innerText;
+        const words = txtValue.split(" ");
+        if(txtValue.toUpperCase().indexOf(filter) > -1) {
+          elements[i].parentElement.style.display = "";
+          if(!current_elements.includes(txtValue)){
+            current_elements.push(txtValue);
           }
-        });
-        if(total == 0 && filter != "") {
-          elements[i].parentElement.style.display = "none";
-          current_elements.splice(i, 1);
+        } else {
+          let total = 0;
+          words.forEach((word) => {
+            if (smart_search(filter.toLowerCase(), word.toLowerCase())) {
+              elements[i].parentElement.style.display = "";
+              if(!current_elements.includes(txtValue)) {
+                current_elements.push(txtValue);
+                total++;
+              }
+            }
+          });
+          if(total == 0 && filter != "") {
+            elements[i].parentElement.style.display = "none";
+            current_elements.splice(i, 1);
+          }
+          total = 0;
         }
-        total = 0;
       }
     }
   }
